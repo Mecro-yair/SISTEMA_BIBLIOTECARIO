76999e2d12c50586281893b4b5ccaeba49887a83
ronald <rchoquehuancar@unjbg.edu.pe>
2024-12-06 22:01:48 -0500

    Agregando clase persona para heredar a usuario y administrador  e implementando sus metodos en este archivo

---
+++ persona.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include "persona.h"
+
+
+using namespace std;
+
+Persona::Persona(string _dni,string _nombre,string _email,int _edad,char _sexo){
+	dni=_dni;
+	nombre=_nombre;
+	email=_email;
+	edad=_edad;
+	sexo=_sexo;
+}
+string Persona::getDni(){
+    return dni;
+}
+
+string Persona::getNombre(){
+    return nombre;
+}
+
+string Persona::getEmail(){
+    return email;
+}
+int Persona::getEdad(){
+	return edad;
+}
+char Persona::getSexo(){
+	return sexo;
+}
+void Persona::mostrarPersona(){
+	cout <<"Dni: "<<dni<<endl;
+	cout<<"Nombre: "<<nombre<<endl;
+	cout<<"Email: "<<email<<endl;
+}
