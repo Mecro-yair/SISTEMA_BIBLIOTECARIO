1022984484893a3cea3775cd07edf3b4e60df979
ronald <rchoquehuancar@unjbg.edu.pe>
2024-12-11 13:31:19 -0500

    declare la funcion de registrarPrestamo traladada del main

--- extras.h
+++ extras.h
@@ -1,5 +1,6 @@
 #ifndef EXTRAS_H
 #define EXTRAS_H
+#include "biblioteca.h"
 
 void imprimirColoreado(const char* texto[], int i);
 void imprimirLogo();
@@ -7,4 +8,7 @@ void resetColor();
 void setColor(int code);
 std::string obtenerFechaHoraActual();
 std::string obtenerFechaDevolucion(int dias_sumar);
+
+
+void registrarPrestamo1(Biblioteca& biblioteca);
 #endif
